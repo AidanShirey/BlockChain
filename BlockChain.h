fedbf5885cdd4d2ff415c8b215ed06df9a868761
Aidan Shirey <aidan.shirey@gmail.com>
2021-04-05 19:09:50 -0700

    adding merkle tree code and templates

--- BlockChain.h
+++ BlockChain.h
@@ -1,19 +1,21 @@
 #pragma once
 #include <string>
+using namespace std;
+
 class BlockChain{
 	Block genesisBlock;
 	int blockCount;
 public:
 	bool initBlockChain();
 	int getBlockCount();
-}
+};
+
 
 class Block {
-	String prev_hash;
-	String timestamp;
+	string prev_hash;
+	string timestamp;
 	// Merkle mRoot;
-	String nonce;
+	string nonce;
 public:
 	bool createBlock();
-}
-
+};
